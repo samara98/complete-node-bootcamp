fc86787a820dcf411aa9d92fb31f97a5cae6ce0c
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-05-05 10:49:04 +0700

    Natours API

---
+++ controllers/userController.js
@@ -0,0 +1,38 @@
+class UserController {
+  static getAllUsers(req, res) {
+    res.status(500).json({
+      status: 'error',
+      message: 'This route is not yet defined!'
+    });
+  }
+
+  static getUser(req, res) {
+    res.status(500).json({
+      status: 'error',
+      message: 'This route is not yet defined!'
+    });
+  }
+
+  static createUser(req, res) {
+    res.status(500).json({
+      status: 'error',
+      message: 'This route is not yet defined!'
+    });
+  }
+
+  static updateUser(req, res) {
+    res.status(500).json({
+      status: 'error',
+      message: 'This route is not yet defined!'
+    });
+  }
+
+  static deleteUser(req, res) {
+    res.status(500).json({
+      status: 'error',
+      message: 'This route is not yet defined!'
+    });
+  }
+}
+
+module.exports = UserController;
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
-const usersRouter = require('./users');
+const usersRouter = require('./usersRouter');
+const toursRouter = require('./toursRouter');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.use('/users', usersRouter);
+router.use((req, res, next) => {
+  console.log('Hello from the middleware 👋');
+  next();
+});
+
+router.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+router.use('/api/v1/users', usersRouter);
+router.use('/api/v1/tours', toursRouter);
 
 module.exports = router;
